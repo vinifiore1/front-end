581ae4ae1158eb1d01e6f916c1423f3955a4cd05
vinifiore <araujovinicius1@hotmail.com>
2022-09-19 21:06:39 -0300

    atualização do height

--- src/components/FullPageNotAuth/styles.ts
+++ src/components/FullPageNotAuth/styles.ts
@@ -3,16 +3,19 @@ import styled from "styled-components";
 export const LoginContainerMain = styled.div`
   display: flex;
   width: 100%;
-  height: 100vh;
+  height: 100%;
 `;
 
 export const LogoContainer = styled.div`
-  height: 100%;
+  height: 100vh;
   width: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
   background: #354f52;
+  @media (max-width: 1400px) {
+    height: 120vh;
+  }
 `;
 export const ContainerChildren = styled.div`
   width: 50%;
